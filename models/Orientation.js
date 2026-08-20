4d0fef1d38070ea5cb8d5912cb6d1cb448e18ec6
Suresh_Pantham <nagagangasureshp@adityauniversity.in>
2026-08-20 09:35:23 +0530

    location removed.

--- models/Orientation.js
+++ models/Orientation.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const orientationSchema = new mongoose.Schema({
   rNo: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  location: { type: String, required: false },
   branch: { type: String, required: true },
   phone: { type: String, required: true },
   attendanceCount: { type: Number, required: true },
